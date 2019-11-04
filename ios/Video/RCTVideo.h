de8c1c58082e6e6da9ba5f95d64df27460f7f8ec
joemewes <joe@4alldigital.com>
2019-11-04 14:23:57 +0000

    fix(delegate): remove unknown delegate for tvOS :bug:

--- ios/Video/RCTVideo.h
+++ ios/Video/RCTVideo.h
@@ -16,7 +16,7 @@
 #if __has_include(<react-native-video/RCTVideoCache.h>)
 @interface RCTVideo : UIView <RCTVideoPlayerViewControllerDelegate, DVAssetLoaderDelegatesDelegate>
 #else
-@interface RCTVideo : UIView <RCTVideoPlayerViewControllerDelegate, AVPictureInPictureControllerDelegate>
+@interface RCTVideo : UIView <RCTVideoPlayerViewControllerDelegate>
 #endif
 
 @property (nonatomic, copy) RCTBubblingEventBlock onVideoLoadStart;
